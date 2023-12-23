757491e80b8d4021b3e31e3dd07d5d1256d70f61
stuent-ananya <s.ananya2626@gmail.com>
2023-12-23 19:17:23 +0530

    update

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('''<h1>WELCOME</h1> <a href="https://youtu.be/uJtoY919sjU?si=8I_-ma4xoWgq3-SV "> mere bhole nath </a>''')
+    return HttpResponse('''<h1>WELCOME</h1> <a href="https://youtu.be/uJtoY919sjU?si=8I_-ma4xoWgq3-SV "> mere bhole nath </a> ''')
 
 
 def about(request):
-    return HttpResponse("<h1>WELCOME IN MY WEBSITE </h1>")
+    return HttpResponse("<h1>WELCOME IN MY WEBSITE </h1>"<a href="about"> back to about </a>)
+
+def firsturl(request):
+    return HttpResponse("<h1>WELCOME IN MY WEBSITE first url </h1>")
 
 
 
