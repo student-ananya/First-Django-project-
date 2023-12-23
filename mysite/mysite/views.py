de7aa2f41220ad78bb3f949ebb2af34f68ed01a6
stuent-ananya <s.ananya2626@gmail.com>
2023-12-23 18:12:44 +0530

    url

---
+++ mysite/mysite/views.py
@@ -0,0 +1,26 @@
+#I have created this file-MY SITE
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('''<h1>WELCOME</h1> <a href="https://youtu.be/uJtoY919sjU?si=8I_-ma4xoWgq3-SV "> mere bhole nath </a>''')
+
+
+def about(request):
+    return HttpResponse("<h1>WELCOME IN MY WEBSITE </h1>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
